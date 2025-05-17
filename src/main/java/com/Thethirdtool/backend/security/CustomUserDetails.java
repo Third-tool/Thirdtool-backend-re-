a7405b12d1904d1b4919f7b0126dafbac3720c9e
junseong kim <-67666411+js-kim-arc@users.noreply.github.com>
2025-05-17 22:40:04 +0900

    [feature/service] 스프링 시큐리티 -> 카카오 로그인 관련 로직 설정하였습니다. -추후 디테일한 설계 더 진행하겠습니다! - userdetail과 userdetailservice 로직 진행

---
+++ src/main/java/com/Thethirdtool/backend/security/CustomUserDetails.java
@@ -0,0 +1,41 @@
+package com.Thethirdtool.backend.security;
+
+import com.Thethirdtool.backend.Member.domain.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    public CustomUserDetails(Member member) {
+        this.member = member;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getKakaoId(); // username은 kakaoId 사용
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+    @Override
+    public boolean isEnabled() { return true; }
+}

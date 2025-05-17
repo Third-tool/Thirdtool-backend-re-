4b331f995955e5c7f55caa6e5760b89c32850622
junseong kim <-67666411+js-kim-arc@users.noreply.github.com>
2025-05-18 02:00:54 +0900

    [feature/application] 카드 컨트롤러 학습 데이터 관련 추가 로직 진행 카드 프리뷰 관련 비즈니스 로직 추가건 컨트롤러에 추가 진행하였습니다!

--- src/main/java/com/Thethirdtool/backend/Card/dto/request/CardStudyResultRequest.java
+++ src/main/java/com/Thethirdtool/backend/Card/dto/request/CardStudyResultRequest.java
@@ -1,4 +1,9 @@
 package com.Thethirdtool.backend.Card.dto.request;
 
-public record CardStudyResultRequest() {
-}
+import com.Thethirdtool.backend.Card.domain.StudyResult;
+import jakarta.validation.constraints.NotNull;
+
+public record CardStudyResultRequest(
+        @NotNull(message = "학습 결과는 필수입니다.")
+        StudyResult result
+) {}
--- src/main/java/com/Thethirdtool/backend/Card/presentation/CardController.java
+++ src/main/java/com/Thethirdtool/backend/Card/presentation/CardController.java
@@ -1,6 +1,8 @@
 package com.Thethirdtool.backend.Card.presentation;
 
 import com.Thethirdtool.backend.Card.application.CardService;
+import com.Thethirdtool.backend.Card.dto.CardStudyViewDto;
+import com.Thethirdtool.backend.Card.dto.request.CardStudyResultRequest;
 import com.Thethirdtool.backend.Card.dto.request.CardUpdateRequest;
 import com.Thethirdtool.backend.Card.dto.response.ApiResponse;
 import com.Thethirdtool.backend.Card.dto.response.CardResponse;
@@ -15,6 +17,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.AccessDeniedException;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -58,6 +61,29 @@ public class CardController {
         validateUser(userId, userDetails);
         return ResponseEntity.ok(ApiResponse.ok(cardService.getCardStudyView(cardId)));
     }
+
+    // 학습 전 프리뷰
+    @GetMapping("/{cardId}/study/preview")
+    public ResponseEntity<ApiResponse<Map<String, String>>> getStudyPreview(
+            @PathVariable Long userId,
+            @PathVariable Long cardId,
+            @AuthenticationPrincipal CustomUserDetails userDetails) throws AccessDeniedException {
+        validateUser(userId, userDetails);
+        return ResponseEntity.ok(ApiResponse.ok(cardService.getStudyPreview(cardId)));
+    }
+
+    // 학습 결과 전송
+    @PostMapping("/{cardId}/study/result")
+    public ResponseEntity<ApiResponse<String>> studyResult(
+            @PathVariable Long userId,
+            @PathVariable Long cardId,
+            @Valid @RequestBody CardStudyResultRequest request,
+            @AuthenticationPrincipal CustomUserDetails userDetails) throws AccessDeniedException {
+        validateUser(userId, userDetails);
+        cardService.processStudyResult(cardId, request.result());
+        return ResponseEntity.ok(ApiResponse.ok("보내는데 성공하였습니다."));
+    }
+
 
 
     // ✅ 공통 검증 메서드 ->>> 공통메서드 뽑을 줄 알기
